2b952297586859c8be5a54d8fe085901bbba966a
Paulo Henrique Rodrigues Grund <paulinhogrund@gmail.com>
2018-04-27 10:48:28 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAMANHO 10
+
+int main()
+{
+    int n[TAMANHO], cont = 0, i[TAMANHO], soma = 0;
+    printf("Digite %i valores inteiros positivos:\n", TAMANHO);
+    while(cont < TAMANHO){
+        printf("Vetor [%i] = ", cont);
+        scanf("%i", &n[cont]);
+        if(n[cont] <= 0)
+            printf("Tente novamente.\n");
+        else{
+            soma += n[cont];
+            i[cont] = cont;
+            cont++;
+        }
+    }
+    printf("///////////////////////////////\n");
+    for(cont = 0; cont < TAMANHO; cont++){
+        printf("[%i] %i\n", i[cont], n[cont]);
+    }
+    printf("///////////////////////////////\n");
+    printf("%i", soma);
+    return 0;
+}
